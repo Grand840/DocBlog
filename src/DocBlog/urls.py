b2918bd81b67612f91ab269a9c0d81505227e8e9
Younous TCHAO <younoustchao@gmail.com>
2025-04-22 21:03:09 +0000

    Ajout de url dans urls.py

--- src/DocBlog/urls.py
+++ src/DocBlog/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import test_de_vue
+from .views import index
 
 urlpatterns = [
+    path('', index, name="index" ),
     path('admin/', admin.site.urls),
-    path('test/', test_de_vue)
 ]
